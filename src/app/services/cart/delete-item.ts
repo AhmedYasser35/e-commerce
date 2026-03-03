6906fe512543b29ab1f9757fc767924f42a3c0be
AhmedYasser35 <ahmedyasserabds@gmail.com>
2026-03-03 02:08:00 +0200

    cookie handle final

--- src/app/services/cart/delete-item.ts
+++ src/app/services/cart/delete-item.ts
@@ -1,12 +1,13 @@
 "use server"
 import { getAccessToken } from "@/schema/access-token";
+import { NextResponse } from "next/server";
 
 export async function delCartItem(productId:string) {
     console.log(productId)
     const token:any = await getAccessToken()
-    if(!token){
-        throw new Error("unauthorized");
-    }
+    if(!token|| typeof token.token !=='string'){
+            return NextResponse.json({error:'unauthorized' , status:401})
+        }
     const response=await fetch(`${process.env.API}/cart/${productId}`,{
         method:'DELETE',
         headers:{
